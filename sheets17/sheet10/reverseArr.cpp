e0370ff6eadf625883542cc05f1280ba863b6d37
Tarekk Mohamed Abdalla <tarekkma@gmail.com>
2018-01-16 10:32:44 +0200

    fix mid17b 4 thanks hossam mostafa

---
+++ sheets17/sheet10/reverseArr.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+
+void reverseArray(int a[],int n){
+    int i = 0;
+    int j = n-1;
+
+    while(i<j){
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+        i++;
+        j--;
+    }
+}
+
+int main(){
+    int a[] = {1,2,3,4,5,6,7,8,9,10};
+
+    reverseArray(a,10);
+
+    for(int i=0;i<10;i++){
+        cout << a[i] << endl;
+    }
+
+
+}
